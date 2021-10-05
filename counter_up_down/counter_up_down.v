aed41a48672d25071a6a6b7aad4e523be3f3585b
Swastik Khuntia <swostikkhuntia@gmail.com>
2021-10-05 22:40:59 +0530

    Updated UP_Down_Counter

--- counter_up_down/counter_up_down.v
+++ counter_up_down/counter_up_down.v
@@ -1,5 +1,6 @@
 //N-bit parameterized up-down counter with synchronous reset
-module counter #(parameter WIDTH = 16)
+//The counter counts up when the input up_down is at logic level '1' and counts down otherwise
+module counter_up_down #(parameter WIDTH = 16)
   (input clk, reset, up_down,
    output reg [WIDTH-1:0] count);
   always @(posedge clk)
--- counter_up_down/counter_up_down_tb.v
+++ counter_up_down/counter_up_down_tb.v
@@ -1,10 +1,9 @@
-
-`include "counter.v"
-module counter_tb #(parameter WIDTH = 2)();
+`include "counter_up_down.v"
+module counter_up_down_tb #(parameter WIDTH = 2)();
   reg clk = 0, reset, up_down;
   wire [WIDTH-1:0] count;
   
-  counter #(WIDTH) uut (clk, reset, up_down, count);
+  counter_up_down #(WIDTH) uut (clk, reset, up_down, count);
   
   always #5 clk = ~clk;
   initial begin
@@ -19,7 +18,7 @@ module counter_tb #(parameter WIDTH = 2)();
   initial begin
     $monitor("Reset = %b, Up_down = %b, Count = %d", reset, up_down, count);
     $dumpfile("test.vcd");
-    $dumpvars(0,counter_tb);
+    $dumpvars(0,counter_up_down_tb);
     #350 $finish;
   end
 endmodule
