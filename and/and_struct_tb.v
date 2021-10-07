f74d934de5a2a413c9445dd37a42540e3914cb65
challatharun <77040227+challatharun@users.noreply.github.com>
2021-10-07 16:23:20 +0530

    and gate
    added and gate as per suggestion

---
+++ and/and_struct_tb.v
@@ -0,0 +1,23 @@
+module and_struct_tb();
+
+reg a,b;
+wire o;
+
+and_struct uut(.a(a), .b(b), .o(o));
+
+initial begin
+
+$dumpfile("and_struct.vcd");
+$dumpvars(0,and_str_tb);
+
+ a = 1'b0;  //intializing the varibales, and it takes only one bit at a time
+ b = 1'b0;
+ #16 $finish; //it will make reset
+end
+
+always #5 a = ~a;
+always #10 b = ~b;
+initial begin
+    $monitor("time = %0t,a =%b,b =%b,o =%b",$time,a,b,o);
+end
+endmodule
