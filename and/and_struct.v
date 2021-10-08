de88ac9ee2680b1680b22de5c8de33d4669bde4a
challatharun <77040227+challatharun@users.noreply.github.com>
2021-10-09 00:21:02 +0530

    AND gate (#16)
    * added AND gate
    
    * OR gate added
    
    * Delete AND directory
    
    * Delete OR directory
    
    * added and
    
    changed the folder name as suggested
    
    * Delete and directory
    
    * and gate
    
    added and gate as per suggestion
    
    * or gate
    
    added or gate as per suggestion

---
+++ and/and_struct.v
@@ -0,0 +1,7 @@
+module and_struct(a,b,o);
+
+input a,b;
+output o;
+
+and(o,a,b);
+endmodule
---
+++ or/or_rtl.v
@@ -0,0 +1,10 @@
+module or_rtl(a,b,o);
+
+input a,b;
+output reg o;
+
+always @(a,b) 
+begin
+o = a|b;    
+end
+endmodule
