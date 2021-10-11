03e2460e04cf2c630484cf2fd434c23e2823222a
Alen Antony <77876851+Anonymous-AAA@users.noreply.github.com>
2021-10-12 00:41:42 +0530

    Demux1x2 (#20)
    * Committed half_adder to the repo
    
    * Half Subtractor Commited
    
    * Committing 1x2 demux
    
    * Delete half_adder_rtl.v
    
    * Delete half_adder_rtl_tb.v
    
    * Delete half_adder_struct.v
    
    * Delete half_adder_struct_tb.v
    
    * Delete half_subtractor_rtl.v
    
    * Delete half_subtractor_rtl_tb.v
    
    * Delete half_subtractor_struct.v
    
    * Delete half_subtractor_struct_tb.v
    
    * Update demux1x2_rtl.v
    
    * Update demux1x2_rtl_tb.v
    
    * Update demux1x2_struct.v
    
    * Update demux1x2_struct_tb.v
    
    * Update demux1x2_struct.v
    
    * Update demux1x2_rtl.v
    
    * Update demux1x2_rtl_tb.v
    
    * Update demux1x2_struct.v
    
    * Update demux1x2_struct_tb.v
    
    Co-authored-by: Adithya Sunil <adithya.edakkadan@students.iiit.ac.in>

---
+++ demux1x2/demux1x2_rtl.v
@@ -0,0 +1,11 @@
+module demux1x2_rtl
+( output reg out0,out1,
+  input  s,in);
+  
+  always@(s,in)
+  begin
+  out0=~((~(in&(~(s&s))))&(~(in&(~(s&s)))));
+  out1=~((~(in&s))&(~(in&s)));
+  end
+  
+  endmodule
